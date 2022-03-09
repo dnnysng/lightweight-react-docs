0ebd7ce1daac7940e54c601441d5c630db9a2474
dseng13 <dseng13@gmail.com>
2022-03-09 00:12:23 -0500

    initial commit

--- src/pages/ecosystem/Ecosystem.js
+++ src/pages/ecosystem/Ecosystem.js
@@ -36,6 +36,27 @@ export default function Ecosystem() {
               <code>{`<Route path="about" element={<About />} />`}</code><br />
               <code>{`</Route>`}</code>
             </li>
+            <li><a href="https://reactrouter.com/docs/en/v6">React Router Docs</a></li>
+          </ul>
+        </Topic>
+        <Topic
+          title="Server-Side Rendering"
+          summary="Opt for server-side rendering if you need to pre-render a page whose data must be fetched at request time."
+        />
+        <Topic
+          title="Client-Side Rendering"
+          summary="If your page contains frequently updating data, and you don't need to pre-render the data, you can fetch the data on the client side."
+        />
+        <Topic
+          title="Static Site Generation"
+          summary="Use this if the data required to render the page is available at build time ahead of a user's request, the data comes from a headless CMS, data is publicly cached, or if the page must be pre-rendered and be very fast."
+        />
+        <Topic
+          title="NextJS"
+          summary="A tool for creating react applications with a lot of features built in such as routing, image optimization, fast refresh, and data fetching."
+        >
+          <ul>
+            <li><p className={style.code}><code>npx create-next-app</code></p></li>
           </ul>
         </Topic>
       </div>
